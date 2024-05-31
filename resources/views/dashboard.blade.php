99ea1dbc515ed749f0c350b2b658343d08279549
halurabiyyu <halurabiyyu@gmail.com>
2024-05-31 14:45:07 +0700

    feat: add template adminlte

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.template')
+
+@section('content')
+
+<div class="card">
+  <div class="card-header">
+    <h3 class="card-title">Halo, apakabar!!!</h3>
+    <div class="card-tools"></div>
+  </div>
+  <div class="card-body">
+    Selamat Datang Semua, Ini adalah halaman utama dari aplikasi ini
+  </div>
+</div>
+@endsection
