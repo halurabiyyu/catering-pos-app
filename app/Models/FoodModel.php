365d9c4063567312cd436467c44112b49afb86d6
halurabiyyu <halurabiyyu@gmail.com>
2024-07-30 21:46:09 +0700

    feat: add seeder and model for food table

--- app/Models/FoodModel.php
+++ app/Models/FoodModel.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class FoodModel extends Model
 {
     use HasFactory;
+
+    protected $table = 'food';
+    protected $primaryKey = 'food_id';
+    protected $fillable = ['food_code', 'food_name', 'food_price', 'food_desc'];
+
+    public $timestamps = true;
+
+    public function category(): BelongsTo{
+        return $this->belongsTo(CategoryModel::class, 'category_id', 'category_id');
+    }
+
 }
--- database/migrations/2024_07_21_134836_add_table_food.php
+++ database/migrations/2024_07_21_134836_add_table_food.php
@@ -11,7 +11,18 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('food', function(Blueprint $table) {
+            $table->id('food_id');
+            $table->string('food_code');
+            $table->string('food_name');
+            $table->string('food_desc');
+            $table->bigInteger('food_price');
+            $table->unsignedBigInteger('category_id');
+            $table->timestamps();
+            
+            $table->foreign('category_id')->references('category_id')->on('category');
+            
+        });
     }
 
     /**
@@ -19,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('food');
     }
 };
