4d716aac9f47d06f8509a904bc88e83d243fd7fc
halurabiyyu <halurabiyyu@gmail.com>
2024-08-13 21:53:25 +0700

    style : add icons for delete

--- resources/views/customer/checkout.blade.php
+++ resources/views/customer/checkout.blade.php
@@ -126,8 +126,13 @@
                                         <span>Title Food</span>
                                         <span class="fw-bold">$Price Food</span>
                                     </div>
-                                    <div class="d-flex justify-content-end">
-                                        <a href="" class="btn btn-danger">Delete</a>
+                                    <div class="d-flex justify-content-end mt-5">
+                                        <a href="" class="text-decoration-none">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#909294" class="bi bi-trash" viewBox="0 0 16 16">
+                                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                                                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                                              </svg>
+                                        </a>
                                     </div>  
                                 </div>
                             </div>
@@ -145,7 +150,7 @@
                                     <span>Total : </span>
                                 </div>
                                 <div class="">
-                                    <span>Rp. 100.000</span>
+                                    <span class="fw-bold">Rp. 100.000</span>
                                 </div>
                             </div>
                             <form action="" method="POST" >
